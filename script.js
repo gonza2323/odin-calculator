35ec0d02119543eff6a7795b54e8204fdd8e6a6a
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2024-12-26 22:16:11 -0300

    Refactor input system and calculator logic

--- script.js
+++ script.js
@@ -4,75 +4,148 @@
 const display = document.querySelector(".display"); 
 const keypad = document.querySelector(".keypad");
 
-add.symbol = "+";
-minus.symbol = "−";
-multiply.symbol = "×";
-divide.symbol = "÷";
-equal.symbol = "";
+const actions = {
+    "equal": () => equal(),
+    "add": () => inputOperation(add),
+    "subtract": () => inputOperation(subtract),
+    "multiply": () => inputOperation(multiply),
+    "divide": () => inputOperation(divide),
+    "clear-all" : () => clearAll(),
+    "clear-entry": () => numericInput("clear-entry"),
+    "add-decimal": () => numericInput("add-decimal"),
+    "negate": () => numericInput("negate"),
+    "0": () => numericInput(0),
+    "1": () => numericInput(1),
+    "2": () => numericInput(2),
+    "3": () => numericInput(3),
+    "4": () => numericInput(4),
+    "5": () => numericInput(5),
+    "6": () => numericInput(6),
+    "7": () => numericInput(7),
+    "8": () => numericInput(8),
+    "9": () => numericInput(9),
+}
+
+const keyboardControls = {
+    "Enter": "equal",
+    "Escape": "clear-all",
+    "Backspace": "clear-entry",
+    ".": "add-decimal",
+    "+": "add",
+    "-": "subtract",
+    "*": "multiply",
+    "/": "divide",
+}
+
+const states = {
+    INPUT_OPERAND: 0,
+    INPUT_OPERATION: 1,
+    RESULT: 2,
+}
 
 
-let numberInMemory;
+let operandA;
+let operandB;
 let currentOperation;
-let awaitingArgument;
+let previousOperation;
+let currentState;
+let displayShouldClear;
 
 
-clearAll();
+function clearAll() {
+    operandA = null;
+    operandB = null;
+    currentOperation = null;
+    previousOperation = null;
+    displayShouldClear = false;
+    currentState = states.INPUT_OPERAND;
+    clearEntry();
+}
 
-
-function getOperationFromSymbol(symbol) {
-    switch (symbol) {
-        case "+": return add;
-        case "−": return minus;
-        case "×": return multiply;
-        case "÷": return divide;
-        case "=": return equal;
+function numericInput(input) {
+    switch (currentState) {
+        case states.RESULT:
+            clearAll();
+        case states.INPUT_OPERATION:
+            clearEntry();
+            currentState = states.INPUT_OPERAND;
+    }
+    
+    switch (input) {
+        case "clear-entry":
+            clearEntry();
+            return;
+        case "negate":
+            inputPlusMinusSign();
+            return;
+        case "add-decimal":
+            inputDecimal();
+            return;
+        default:
+            inputNumber(input);
+            return;
     }
 }
 
-function readNumberFromDisplay() {
-    return parseFloat(display.textContent);
+function inputOperation(operation) {
+    switch (currentState) {
+        case states.INPUT_OPERATION:
+            display.textContent = display.textContent.replace(currentOperation.symbol, '');
+            break;
+        case states.INPUT_OPERAND:
+        case states.RESULT:
+            if (currentOperation) {
+                operandB = readNumberFromDisplay();
+                operate(currentOperation);
+            } else {
+                operandA = readNumberFromDisplay();
+            }
+            currentState = states.INPUT_OPERATION;
+            break;
+    }
+
+    currentOperation = operation;
+    display.textContent += operation.symbol;
+    displayShouldClear = true;
 }
 
-function writeNumberToDisplay(number) {
-    display.textContent = number;
+function equal() {
+    switch (currentState) {
+        case states.INPUT_OPERATION:
+            writeNumberToDisplay(operandA);
+            break;
+        case states.INPUT_OPERAND:
+            operandB = readNumberFromDisplay()
+        case states.RESULT:
+            const operation = currentOperation || previousOperation;
+            if (operandB === null)
+                operandB = readNumberFromDisplay();
+        
+            operate(operation);
+            break;
+    }
+        
+    currentOperation = null;
+    displayShouldClear = true;
+    currentState = states.RESULT;
 }
 
 function operate(operation) {
-    if (awaitingArgument) {
-        display.textContent = display.textContent.replace(currentOperation.symbol, '') + operation.symbol;
-        currentOperation = operation;
-        return;
-    }
-
-    const numberInDisplay = readNumberFromDisplay();
-    const result = currentOperation(numberInMemory, numberInDisplay);
-    numberInMemory = result;
-    writeNumberToDisplay(result);
-    
-    currentOperation = operation;
-    display.textContent += operation.symbol;
-    awaitingArgument = true;
+    const result = operation(operandA, operandB);
+    operandA = result;
+    writeNumberToDisplay(result); 
+    previousOperation = operation;
 }
 
-function add(a, b) {
-    return a + b;
-}
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
-function minus(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function equal(a, b) {
-    return b;
-}
+add.symbol = "+";
+subtract.symbol = "−";
+multiply.symbol = "×";
+divide.symbol = "÷";
 
 function inputNumber(number) {
     if (display.textContent === "0" ||
@@ -93,12 +166,13 @@ function clearEntry() {
     display.textContent = "0";
 }
 
-function clearAll() {
-    currentOperation = equal;
-    numberInMemory = 0;
-    awaitingArgument = false;
-    clearEntry();
-    // todo
+
+function readNumberFromDisplay() {
+    return parseFloat(display.textContent);
+}
+
+function writeNumberToDisplay(number) {
+    display.textContent = number;
 }
 
 function inputPlusMinusSign() {
@@ -108,74 +182,44 @@ function inputPlusMinusSign() {
         display.textContent = "-" + display.textContent;
 }
 
-function isNumericInput(input) {
-    const numericButtons = ["CE", "±", "."];
-    const number = parseInt(input);
-
-    return numericButtons.includes(input) || !isNaN(number);
+function performAction(actionName) {
+    const action = actions[actionName];
+    if (action) action();
 }
 
-function onButtonPress(event) {
-    const key = event.target.textContent;
+function onButtonClick(event) {
+    event.stopPropagation();
     
-    if (!key) return;
-
-    if (key === "AC") {
-        clearAll();
-        return;
-    }
-    
-    const operation = getOperationFromSymbol(key);
-    if (operation) {
-        operate(operation);
-        return;
-    }
-
-    if (isNumericInput(key)) {
-        if (awaitingArgument) {
-            clearEntry();
-            awaitingArgument = false;
-        }
-
-        switch (key) {
-            case "CE":
-                clearEntry();
-                return;
-            case "±":
-                inputPlusMinusSign();
-                return;
-            case ".":
-                inputDecimal();
-                return;
-            default:
-                inputNumber(parseInt(key));
-                return;
-        }
-    }
+    const action = event.target.dataset.action;
+    performAction(action);
 }
 
 function onKeyPressDown(event) {
+    event.stopPropagation();
+    
     const key = event.key;
-    const button = document.querySelector(`button[data-key="${key}"]`)
 
-    if (!button) return;
+    const action = keyboardControls[key] || key;
+    performAction(action);
 
-    button.classList.add("active");
-    button.click();
+    const button = document.querySelector(`button[data-action="${action}"]`);
+    if (button) button.classList.add("active");
     
     if (key === '/') event.preventDefault();
 }
 
 function onKeyPressUp(event) {
+    event.stopPropagation();
+
     const key = event.key;
-    const button = document.querySelector(`button[data-key="${key}"]`);
-
-    if (!button) return;
-
-    button.classList.remove("active");
+    const action = keyboardControls[key] || key;
+    const button = document.querySelector(`button[data-action="${action}"]`);
+    if (button) button.classList.remove("active");
 }
 
 
-keypad.addEventListener("click", onButtonPress);
+clearAll();
+
+keypad.addEventListener("click", onButtonClick);
 window.addEventListener("keydown", onKeyPressDown);
 window.addEventListener("keyup", onKeyPressUp);
